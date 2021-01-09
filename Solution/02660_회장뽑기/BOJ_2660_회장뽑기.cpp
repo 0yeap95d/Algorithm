88222ea729bd4cf852f605154fee5d761874294b
0yeap95d <tjdwob@gmail.com>
2021-01-09 12:58:29 +0900

    2021.01.09 | 2660 |  회장뽑기

--- Solution/02660_회장뽑기/BOJ_2660_회장뽑기.cpp
+++ Solution/02660_회장뽑기/BOJ_2660_회장뽑기.cpp
@@ -58,7 +58,7 @@ int main() {
 	cout << minScore << " " << cnt << "\n";
 
 	// 회장 후보 출력
-	for (int i = 0; i < N; i++) {
+	for (int i = 1; i <= N; i++) {
 		if (ans[i] == minScore) cout << i << " ";
 	}
 

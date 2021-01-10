e82ea28414998dd310797a60f3cfaaf6ff38c382
0yeap95d <tjdwob@gmail.com>
2021-01-10 21:38:11 +0900

    2021.01.10 | 18883 | N M 찍기

---
+++ Solution/18883_N M 찍기/BOJ_18883_N M 찍기.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int N, M;
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cout << (i * M) + j + 1;
+			if (j != M - 1) cout << " ";
+		}
+		if (i != N - 1) cout << "\n";
+	}
+
+	return 0;
+}
